5f33f4ffa699f2b3e330aa7334bcfccb4588694d
Frederic Font <frederic.font@upf.edu>
2020-03-16 10:59:45 +0100

    Fix generated tag index file naming

--- _plugins/_tag_gen.rb
+++ _plugins/_tag_gen.rb
@@ -4,7 +4,7 @@ module Jekyll
       @site = site
       @base = base
       @dir = dir
-      @name = ''
+      @name = 'index.html'
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
